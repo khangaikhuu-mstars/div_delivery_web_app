69cea68a1e386994c335b95da6bcdacb8b4e64a7
Angarag Otgonbayar <angarag14@gmail.com>
2021-12-27 21:51:57 +0800

    #6_Changed my css name

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import './css/angarag.css';
 import App from './Component/App';
 import 'bootstrap/dist/css/bootstrap.css';
 
