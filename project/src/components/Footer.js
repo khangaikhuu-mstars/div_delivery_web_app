479d53e1c5279bb2c15de8a18524aa213417bbfc
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2021-12-27 15:55:07 +0800

    #5 [add] footer.js file done

---
+++ project/src/components/Footer.js
@@ -0,0 +1,60 @@
+
+import img1 from '../data/logo.png'
+import img2 from '../data/facebook.png'
+import img3 from '../data/instagram.png'
+import img4 from '../data/twitter.png'
+export default function Footer() {
+    return(
+        <div className="main-footer">
+            <div className="container pt-4">
+                <a href=""><img className='pe-2' src={img1}/><b>Food Delivery</b></a>
+              <div className="row list-unstyle pt-4">
+                  {/* menu */}
+                  <div className="col">
+                     <ul className="list-style fw-bold p-0">
+                         <li><a>Нүүр</a></li>
+                         <li><a>Хоолны цэс</a></li>
+                         <li><a>Хүргэлтийн бүс</a></li>
+                     </ul>
+                  </div>
+                    {/* contact us */}
+                  <div className="col">
+                        <a href=""><p className='fw-bold text-white'>Холбоо барих </p></a>
+                  <ul className="list-style2 text-white">
+                         <li>(976) 77123456</li>
+                         <li>(976) 99489843</li>
+                     </ul>
+                  </div>
+              </div>
+
+              <div className="social-icons">
+                  <a href=""><img src={img2} alt="" /></a>
+                  <a href=""><img src={img3} alt="" /></a>
+                  <a href=""><img src={img4} alt="" /></a>
+              </div>
+                  <hr className='bg-white border'/>
+              <div className='white-border'>
+
+              </div>
+
+              <div className='info'>
+                <ul className='text-white list-unstyled'>
+                    <li>Нууцлалын бодлого</li>
+                    <li>Үйлчилгээний нөхцөл</li>
+                </ul>
+              </div>
+
+            </div>
+            <div className='row'>
+                <div className='col'>
+
+                </div>
+                <div className='col'>
+               <p className='Copyright'>© 2020 MStars Foods LLC </p>
+                </div>
+            </div>
+             
+        </div>
+    )
+}
+
