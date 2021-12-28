44bc4a5dccf29f75c8c08827aacdf4428c8c24c9
lab_1_0 <lab_1_02@lab-1-02s-iMac.local>
2021-12-28 14:07:25 +0800

    #2 Adding menu component

--- project/src/App.js
+++ project/src/App.js
@@ -1,24 +1,11 @@
-
-import './App.css';
+import Web_Menu_Component from './Web_Menu_Component';
+import Menu_Component from './Menu_Component';
+import './css/App.css';
+import './css/menu.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web_Menu_Component/>
   );
 }
 
--- project/src/Menu_Component.js
+++ project/src/Menu_Component.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+import './css/menu.css';
+import './css/App.css';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 
 function Menu_Component() {
-  return (
-    <div className="">
-
+return (
+  <Navbar bg="light" expand={false}>
+  <Container fluid>
+    <Navbar.Toggle aria-controls="Navbar" />
+    <div className="logo">
+      <img src="food1.png" alt="" />
+      <div className="food">Food Delivery</div>
     </div>
-  );
+    <div className="searchAndBusket">
+      <div className="search"><a href="#"><img src="search.png" alt="" /></a></div>
+      <div className="busket"><a href="#"><img src="busket.png" alt="" /></a></div>
+    </div>
+    
+  </Container>
+</Navbar>
+);
 }
 
 export default Menu_Component;
