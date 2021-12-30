0697c93f48244210235b6a18a3d7894358b81043
Mstars_Lab1_11 <mstars_lab1_11@Mstars-Lab1-11s-iMac.local>
2021-12-30 11:39:39 +0800

    #6 fixed comments

--- project/src/App.js
+++ project/src/App.js
@@ -1,20 +1,16 @@
 import MenuComponent from './component/MenuComponent';
 import Slider from './component/Slider';
 import Footer from "./component/Footer";
-import Button from "./component/Button"
-import SeeAll from "./component/SeeAllButton"
 import './css/App.css';
 import './css/menu.css';
 import "./css/footer.css";
-import "./css/button.css"
+import "./css/button.css";
 
 
 function App() {
   return (
     <div>
       <MenuComponent/>
-      <Button value="sda" className=" my-5 active-button w-25"/>
-      <SeeAll />
       <Slider />
       <Footer />
     </div>
