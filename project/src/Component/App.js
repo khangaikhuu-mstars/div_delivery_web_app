4b78b1a61d3e9793afd64b1633a5dc9c25adb280
Mstars_Lab1_11 <mstars_lab1_11@Mstars-Lab1-11s-iMac.local>
2021-12-30 09:40:39 +0800

    #6 done

--- project/src/Component/App.js
+++ project/src/Component/App.js
@@ -1,52 +1,8 @@
 import React from "react";
-import Buttons from "./Button";
 import "../css/App.css";
-import { Col, Container } from "react-bootstrap";
-import Tab from "./Tab";
-import Cards from "./Card";
-import { useEffect, useState } from "react";
-import SeeAllButton from "./SeeAllButton";
-import axios from "axios";
 
 function App() {
-  const [foods, setFoods] = useState([]);
-  useEffect(() => {
-    const axios = require("axios");
-    axios.get("../data/foods.json");
-  }, [Cards]);
-
-  return (
-    <div>
-      <Container>
-        <div id="buttons" className="mt-5">
-          <Buttons value="default" className="default my-2" />
-          <Buttons value="angarag" className="active-button my-2" />
-          <Buttons value="default" className="noBorder my-2" />
-          <SeeAllButton />
-        </div>
-      </Container>
-      <div className="mt-5" id="tabs">
-        <Tab />
-      </div>
-      <div className="container">
-        <div id="cards">
-          <div className="row space-between align-items-center">
-            {foods.map((food) => {
-              return (
-                <Col xs={6} lg={3}>
-                  <Cards
-                    name={food.name}
-                    price={food.price}
-                    thumbnail={food.thumb_img}
-                  />
-                </Col>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
