1d6ba70e3ecdfb65ef8742b4b0315a6d416cb75b
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-01-03 15:06:28 +0800

    #25 finished

--- project/src/App.js
+++ project/src/App.js
@@ -11,15 +11,16 @@ import "./css/TextField.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/tab.css'
 import Category from './component/Category'
+import MainDishMenu from "./component/MainDishMenu";
 
 function App() {
   return (
     <div>
       <MenuComponent/>
       <Slider />
+      <Category />
       <Footer />
-      <Cards/>
-      <Category/>
+      
     </div>
   )
 }
