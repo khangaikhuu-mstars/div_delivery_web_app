7cf41014c864e92f393e540aef2a40b1f71fe78d
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-01-03 10:50:09 +0800

    #24 [start route]

--- project/src/App.js
+++ project/src/App.js
@@ -1,23 +1,24 @@
-import Slider from "./component/Slider"
-import MenuComponent from './component/MenuComponent';
+import Slider from "./component/Slider";
+import MenuComponent from "./component/MenuComponent";
 import Footer from "./component/Footer";
-import Tab from './component/Tab'
-import './css/App.css';
-import './css/menu.css';
+import Home from "./component/Home"
+import "./css/App.css";
+import "./css/menu.css";
 import "./css/footer.css";
-import "./css/TextField.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/TextField.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/button.css";
-import './css/tab.css'
-import "./css/button.css"
+import "./css/tab.css";
+import "./css/button.css";
+import { BrowserRouter, Route } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
-    <div>
-      <MenuComponent />
-      <Slider />
-      <Footer />
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Home}/>
+      </div>
+    </BrowserRouter>
+  );
 }
 export default App;
---
+++ project/src/Component/Home.js
@@ -0,0 +1,15 @@
+import MenuComponent from "./MenuComponent";
+import Slider from "./Slider";
+import Footer from "./Footer";
+
+
+function App() {
+    return (
+        <div>
+          <MenuComponent />
+          <Slider />
+          <Footer />
+        </div>
+    );
+  }
+  export default App;
