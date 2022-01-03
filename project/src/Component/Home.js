56021d9351ca45dc32bd812d2b496c784b64fd21
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-01-03 16:40:34 +0800

    [fixed] footer.css and sales category plus

--- project/src/Component/Home.js
+++ project/src/Component/Home.js
@@ -1,31 +1,16 @@
-import Category from "./Category-container";
+import Category from "./Category";
 import Slider from "./Slider";
-<<<<<<< HEAD
 import InfoPanelComponent from "./InfoPanelComponent"
+
 function Home() {
   return (
     <div>
       <Slider />
       <InfoPanelComponent />
-      <Category/>
+      <Category />
     </div>
   );
 }
 export default Home;
 
  
-=======
-import InfoPanelComponent from "./InfoPanelComponent";
-import Category from "./Category"
-
-function Home() {
-    return (
-        <div>
-          <Slider />
-          <InfoPanelComponent />
-          <Category/>
-        </div>
-    );
-  }
-export default Home;
->>>>>>> dc888005d4586ddd85445d819ba8a6582269e885
