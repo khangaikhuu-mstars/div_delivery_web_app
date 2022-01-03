cae8766f51f648a0beec9988dbf5f4cc0863c2fe
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-01-03 17:06:22 +0800

    #32 [fixed] home.js

--- project/src/Component/Home.js
+++ project/src/Component/Home.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import Category from "./Category";
-import Slider from "./Slider";
-import InfoPanelComponent from "./InfoPanelComponent"
-
-function Home() {
-  return (
-    <div>
-      <Slider />
-      <InfoPanelComponent />
-      <Category />
-    </div>
-  );
-}
-export default Home;
-
- 
-=======
 import Slider from "./Slider";
 import InfoPanelComponent from "./InfoPanelComponent";
 import Category from "./Category"
@@ -30,4 +12,3 @@ function Home() {
     );
   }
 export default Home;
->>>>>>> c1823c5af56d0cbe9903bf01dcbfe7f7cc43af92
