d57e09442f63ffa5efb02442fe10d17f46063e8a
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-01-03 16:18:47 +0800

    [REF] fixing wrong merge in home.js

--- project/src/Component/Home.js
+++ project/src/Component/Home.js
@@ -1,20 +1,5 @@
 import Category from "./Category-container";
 import Slider from "./Slider";
-<<<<<<< HEAD
-import InfoPanelComponent from "./InfoPanelComponent"
-function Home() {
-  return (
-    <div>
-      <Slider />
-      <InfoPanelComponent />
-      <Category/>
-    </div>
-  );
-}
-export default Home;
-
- 
-=======
 import InfoPanelComponent from "./InfoPanelComponent";
 import Category from "./Category"
 
@@ -28,4 +13,3 @@ function Home() {
     );
   }
 export default Home;
->>>>>>> dc888005d4586ddd85445d819ba8a6582269e885
