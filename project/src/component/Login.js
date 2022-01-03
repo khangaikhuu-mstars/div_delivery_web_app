b89c77b829e335016a77f34e8e1e94381ecf0280
lab_1_14 <lab_1_14@lab-1-14s-iMac.local>
2022-01-03 10:31:53 +0800

    #7 add little bit exchange

--- project/src/component/Login.js
+++ project/src/component/Login.js
@@ -6,26 +6,28 @@ import "../css/TextField.css"
 
 const Login = () => {
   return (
-    <Form>
-      <Form.Label className="reBorder" ></Form.Label>
-      <Form.Label className="recoverPass" >НЭВТРЭХ</Form.Label>
-      <Form.Group className="mb-3" controlId="formEmail">
-        <Form.Label className="registerName">И-Мейл</Form.Label>
-        <Form.Control className="formSize" type="email" placeholder="И-Мейл хаягаа оруулна уу." />
-      </Form.Group>
-      <Col xs="auto">
-        <Form.Group className="mb-3 " >
-          <Form.Label className="registerName">Нууц үг</Form.Label>
-          <InputGroup className="mb-2">
-            <Form.Control className="formSize" type="password" controlId="formPassword" placeholder="Нууц үгээ оруулна уу." />
-            <InputGroup.Text className="rightIcon"><a href="#"></a></InputGroup.Text>
-          </InputGroup>
-          <Form.Text >
-            <a href="#" className="text-mute">Нууц үгээ мартсан уу.</a>
-          </Form.Text>
+    <div className="App-header1">
+      <Form >
+        <Form.Label className="reBorder" ></Form.Label>
+        <Form.Label className="recoverPass" >НЭВТРЭХ</Form.Label>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label className="registerName">И-Мейл</Form.Label>
+          <Form.Control className="formSize" type="email" placeholder="И-Мейл хаягаа оруулна уу." />
         </Form.Group>
-      </Col>
-    </Form>
+        <Col xs="auto">
+          <Form.Group className="mb-3 " >
+            <Form.Label className="registerName">Нууц үг</Form.Label>
+            <InputGroup className="mb-2">
+              <Form.Control className="formSize" type="password" controlId="formPassword" placeholder="Нууц үгээ оруулна уу." />
+              <InputGroup.Text className="rightIcon"><a href="#"></a></InputGroup.Text>
+            </InputGroup>
+            <Form.Text >
+              <a href="#" className="text-mute">Нууц үгээ мартсан уу.</a>
+            </Form.Text>
+          </Form.Group>
+        </Col>
+      </Form>
+    </div>
   )
 }
 
