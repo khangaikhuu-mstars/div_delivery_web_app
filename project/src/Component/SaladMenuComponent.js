e5db8cedf299b0173269899132ecf651f9890c5c
Mstars_lab1_20 <mstarsacademy@MStarss-iMac-1.local>
2022-01-04 10:10:44 +0800

    #26 change name and style

--- project/src/Component/SaladMenuComponent.js
+++ project/src/Component/SaladMenuComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Cards from "./Card";
-function SaladMenuComponent() {
+function SaladMenu() {
     const [mainMenu, setMainMenu] = useState([])
     useEffect(() => {
         fetch("../data/foods.json")
@@ -27,4 +27,4 @@ function SaladMenuComponent() {
         </div>
     )
 }
-export default SaladMenuComponent;
+export default SaladMenu;
