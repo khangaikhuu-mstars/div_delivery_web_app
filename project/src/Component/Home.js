25a526048b5c48d1a097708016e54f25d11482d3
lab_1_0 <lab_1_02@lab-1-02s-iMac.local>
2022-01-10 10:58:06 +0800

    #48 modified

--- project/src/Component/Home.js
+++ project/src/Component/Home.js
@@ -1,6 +1,6 @@
 import Slider from "./Slider";
-import InfoPanelComponent from "./InfoPanelComponent";
-import Category from "./Category";
+import InfoPanelComponent from "./component/InfoPanelComponent";
+import Category from "./component/Category";
 
 function Home() {
   return (
--- project/src/Component/SaladMenuComponent.js
+++ project/src/Component/SaladMenuComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Cards from "./Card";
+
 function SaladMenu() {
   const [mainMenu, setMainMenu] = useState([]);
   useEffect(() => {
