e498c88dd8f68c74ab957f50bb69af40bab19f2e
Mstars_Lab1_11 <mstars_lab1_11@Mstars-Lab1-11s-iMac.local>
2022-01-17 10:47:10 +0800

    Deleted context that is not being used

--- project/src/index.js
+++ project/src/index.js
@@ -7,13 +7,10 @@ import "./css/Area.css"
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './css/modal.css'
-import ModalProvider from './context/ModalContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ModalProvider>
       <App />
-    </ModalProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
